794009555b3c1e4f24f4d55a404c5059c5180773
johnvibar <jmichael@captivatechat.com>
2023-10-12 18:19:03 +0300

    add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <ion-header class="explore-header">
+    <ion-img src="../../public/assets/img/explore/headerbar.svg"></ion-img>
+  </ion-header>
+</template>
+
+<script setup>
+import {
+  IonHeader,
+  IonImg,
+} from "@ionic/vue";
+</script>
