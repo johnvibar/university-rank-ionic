0fc45bac8fca12f115b25ada56f34b23d524aa0b
johnvibar <jmichael@captivatechat.com>
2023-10-20 22:23:51 +0300

    updating profile step 1

--- src/pages/explore/ExploreProfileStep1.vue
+++ src/pages/explore/ExploreProfileStep1.vue
@@ -2,16 +2,42 @@
   <ion-page>
     <Header />
     <ion-content>
-
+      <ion-title>Step 1: More about your biography</ion-title>
+      <ion-card class="card-container">
+        <ion-grid>
+          <ion-row class="ion-align-items">
+            <ion-row class="card-list">
+              <ion-img src="/assets/img/icons/icon-metro-qrcode.svg"></ion-img>
+              <ion-label>My QR Code</ion-label>
+            </ion-row>
+          </ion-row>
+        </ion-grid>
+      </ion-card>
+      <ion-card class="card-container">
+        <ion-grid>
+          <ion-row class="ion-align-items">
+            <ion-row class="card-list">
+              <ion-img src="/assets/img/icons/icon-metro-qrcode.svg"></ion-img>
+              <ion-label>My QR Code</ion-label>
+            </ion-row>
+          </ion-row>
+        </ion-grid>
+      </ion-card>
+      <ion-button expand="block">Step 2 Where to study?</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup>
-  import {
-    IonPage,
-    IonContent,
-  } from '@ionic/vue';
+import {
+  IonPage,
+  IonContent,
+  IonCard,
+  IonGrid,
+  IonRow,
+  IonLabel,
+  IonImg,
+} from '@ionic/vue';
 
-  import Header from '@/components/Header.vue';
+import Header from '@/components/Header.vue';
 </script>
